51b30a705e13b21bfec35674edfd85f7cdbc8a7f
Moises Delgado <moises.f.delgado@gmail.com>
2023-04-14 18:06:01 -0400

    Using db_name rather than name as the latter is depreciated

---
+++ vars.tf
@@ -0,0 +1,7 @@
+variable "engine" {}
+variable "engine_version" {}
+variable "instance_class" {}
+variable "db_name" {}
+variable "username" {}
+variable "password" {}
+variable "parameter_group_name" {}
