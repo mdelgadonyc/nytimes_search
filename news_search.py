f965e25d7aca8b73ac8e41f4098d76718c856c31
Moises <mdelgado@example.com>
2023-04-17 10:46:09 -0400

    Added the execute query function

--- news_search.py
+++ news_search.py
@@ -14,6 +14,15 @@ def create_connection(path):
 
     return connection
 
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
 logging.basicConfig(level=logging.DEBUG, format=' %(levelname)s - %(message)s')
 
 # Obtain NYTimes RSS Feed
