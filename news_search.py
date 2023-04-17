c2261f09b8c22a7b6406ecf84aa8f2a8ab036b32
Moises <mdelgado@example.com>
2023-04-17 11:41:35 -0400

    Added queries for the necessary tables

--- news_search.py
+++ news_search.py
@@ -43,3 +43,49 @@ if page.status_code == requests.codes.ok:
             print(title.text)
 
 connection = create_connection("nyt_sqlitedb")
+
+create_users_table = """
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        age INTEGER,
+        gender TEXT,
+        nationality TEXT
+    );
+"""
+
+execute_query(connection, create_users_table)
+
+create_posts_table = """
+    CREATE TABLE IF NOT EXISTS posts(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+    );
+"""
+
+execute_query(connection, create_posts_table)
+
+create_comments_table = """
+    CREATE TABLE IF NOT EXISTS comments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    text TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    post_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
+    );
+"""
+
+create_likes_table = """
+    CREATE TABLE IF NOT EXISTS likes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    post_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
+    );
+"""
+
+execute_query(connection, create_comments_table)
+execute_query(connection, create_likes_table)
