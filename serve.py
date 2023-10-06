936ccc230baaa9377dcaa2f4929b1928b0c40315
mdelgadonyc <moises.f.delgado@gmail.com>
2023-10-06 15:51:00 -0400

    Fixes two typos

--- serve.py
+++ serve.py
@@ -34,9 +34,9 @@ def create_connection(path):
     connection = None
     try:
         connection = sqlite3.connect(path)
-        print("Connect to SQLite DB succuessful")
+        print("Connect to SQLite DB successful")
     except Error as e:
-        print(f"The error '{e}' occured")
+        print(f"The error '{e}' occurred")
 
     return connection
 
